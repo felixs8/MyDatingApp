80f276c9d926da79c23834db670b4526e3c5c7ea
Felix Schmidt <felix_schmidt_85@yahoo.de>
2021-06-08 23:20:13 +0200

    register added, end of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
@@ -12,11 +11,9 @@ export class AppComponent implements OnInit {
   title = 'My Dating App';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
   
   ngOnInit(): void {
-    this.getUsers();
-    this.setCurrentUser();
   }
 
   setCurrentUser() {
@@ -24,12 +21,6 @@ export class AppComponent implements OnInit {
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
-  }
+
 
 }
